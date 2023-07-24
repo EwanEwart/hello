75ae191c78427718b1b054af415d0ca41cbe038e
EwanEwart@HotMail.Co.UK <EwanEwart@HotMail.Co.UK>
2023-07-24 08:56:13 +0200

    + square

--- main.cpp
+++ main.cpp
@@ -2,8 +2,16 @@
 
 using namespace std;
 
+double square(double d)
+{
+    return d*d;
+}
+
 int main()
 {
     auto version {11};
     cout << "C++" << version << " namespace" << endl;    
+    
+    auto d {3.1415};
+    cout << d << " * " << d << " = " << square(d) << endl;
 }
